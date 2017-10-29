803c930458fba780f8e69e33c9ded7378a26e6ff
dariashifrina <dshifrina@stuy.edu>
2017-10-29 18:40:16 -0400

    woo

--- randomdata.c
+++ randomdata.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h> 
+#include <fcntl.h>
 
 int random_num(){
-  int rannum = open("/dev/random", "O_RDONLY", 111);
+  int rannum;
+  int place = open("/dev/random", O_RDONLY);
+  read(place, &rannum, sizeof(int));
+  if(errno){
+    printf("Errno: %d\n", errno);
+    printf("Error Message: %s\n", strerror(errno));
+  }
+
+  close(place);
   return rannum;
 }
 
@@ -12,13 +23,23 @@ int main(){
   printf("Generating random numbers:\n");
   int i;
   for(i = 0; i<10; i++){
-    int wow = random_num();
-    blah[i] = wow;
-    printf("random %d: %d\n", i, wow);
+    blah[i] = random_num();
+    printf("%d\n",blah[i]);
   }
   //write array to file
+  int writefile = open("array", O_WRONLY | O_CREAT);
+  write(writefile, blah, sizeof(int) * 10);
+  close(writefile);
   //read file into different array
   int wow[10];
+  int transferfile = open("array", O_RDONLY);
+  read(transferfile, wow, sizeof(int) * 10);
+  close(transferfile);
   //print content of second arr
+  int counter;
+  printf("Checking if second array matches:\n");
+  for(counter = 0; counter < 10; counter++){
+    printf("%d\n", wow[counter]);
+  }
   return 0;
 }
